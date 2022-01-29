263fd08d4f251b721cc37446521231ad3e30545b
malfadhli <moefna93@gmail.com>
2022-01-29 11:53:33 -0500

    Feature Sun with Errors to be resolved

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -8,6 +8,10 @@ public class Main {
         milkyWay.sun=1;
         milkyWay.moon=2;
         milkyWay.planets=3;
-        
+
+        SUN Solana = new SUN();
+        Solana.Heat="4567kj";
+        Solana.radius="83247202km";
+        Solana.temp="49587c";
     }
 }
